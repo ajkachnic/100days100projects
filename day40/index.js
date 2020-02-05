24b009d5a27be9e42794ec9d704f9f82cb98eec2
Andrew Kachnic <ajkachnic@protonmail.com>
2020-02-04 21:49:18 -0500

    day 40

---
+++ day40/index.js
@@ -0,0 +1,25 @@
+var c = document.getElementById("myCanvas");
+c.setAttribute('width', Math.round(window.innerWidth) + "px")
+c.setAttribute('height', Math.ceil(window.innerHeight) + "px")
+let mousedown = false
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+let mouse = {x: 0, y: 0}
+c.addEventListener('mousedown', e =>{
+    mousedown = true
+    mouse = {x: e.offsetX, y: e.offsetY}
+
+})
+c.addEventListener('mouseup', () => {
+    mousedown = false
+})
+setInterval(() => {
+    if(mousedown) {
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(mouse.x, mouse.y, Math.round(Math.random() * 100), 0, 2 * Math.PI);                  
+        ctx.stroke(); 
+    }
+    
+    
+}, 1000 / 30)
