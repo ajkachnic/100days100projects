0183c1f3902fb2a49992763f6e4a4e7d2f0ae8d5
Andrew Kachnic <ajkachnic@protonmail.com>
2020-03-13 21:14:40 -0400

    New Day: Day 78
    I made a color palette and added some transitions

---
+++ day78/index.js
@@ -0,0 +1,8 @@
+let root = document.documentElement;
+let color = document.querySelectorAll(".color")
+
+color[0].innerHTML = `<p>#ff326f</p>`
+color[1].innerHTML = `<p>#5caffd</p>`
+color[2].innerHTML = `<p>#67f786</p>`
+color[3].innerHTML = `<p>#333333</p>`
+color[4].innerHTML = `<p>#e48bff</p>`
